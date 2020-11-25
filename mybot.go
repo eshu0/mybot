5667fc3870339e47076501083474b5454c6df8ec
eshu0 <willsawesometoolkit@gmail.com>
2020-11-25 21:12:38 +0000

    Update mybot.go

--- mybot.go
+++ mybot.go
@@ -1,24 +1,21 @@
 package mybot
 
 import (
-
 	"fmt"
-	"os"
+
 	"github.com/stianeikeland/go-rpio"
 )
 
 type MyBot struct {
-
-	Pin7 *rpio.Pin
+	Pin7  *rpio.Pin
 	Pin11 *rpio.Pin
 	Pin13 *rpio.Pin
 	Pin15 *rpio.Pin
-
 }
 
-func NewMyBot() *MyBot{
+func NewMyBot() *MyBot {
 
-	mbot := MyBot{}
+	mbot := &MyBot{}
 	fmt.Println("opening gpio")
 	err := rpio.Open()
 	if err != nil {
@@ -26,43 +23,42 @@ func NewMyBot() *MyBot{
 	}
 
 	// 4 is 7
-	mbot.Pin7 = rpio.Pin(4)
+	mbot.Pin7 = &rpio.Pin(4)
 	mbot.Pin7.Output()
 
 	// 11 is 17
-	mbot.Pin11 := rpio.Pin(17)
+	mbot.Pin11 = &rpio.Pin(17)
 	mbot.Pin11.Output()
 
 	//pin13 is 27
-	mbot.Pin13 := rpio.Pin(27)
+	mbot.Pin13 = &rpio.Pin(27)
 	mbot.Pin13.Output()
 
 	// pin 15 is 22
-	mbot.Pin15 := rpio.Pin(22)
+	mbot.Pin15 = &rpio.Pin(22)
 	mbot.Pin15.Output()
 
 	return mbot
 
 }
 
-
-func (bot *MyBot) Forward(){
+func (bot *MyBot) Forwards() {
 	fmt.Println("Forwards!")
 	bot.Pin15.High()
 	bot.Pin11.High()
 	bot.Pin13.Low()
-	bot.Pin7.Low()	
+	bot.Pin7.Low()
 }
 
-func (bot *MyBot) Backwards(){
+func (bot *MyBot) Backwards() {
 	fmt.Println("Backwards!")
 	bot.Pin15.Low()
 	bot.Pin11.Low()
 	bot.Pin13.High()
-	bot.Pin7.High()	
+	bot.Pin7.High()
 }
 
-func (bot *MyBot) Stop(){
+func (bot *MyBot) Stop() {
 	fmt.Println("Ending....")
 	bot.Pin15.Low()
 	bot.Pin11.Low()
@@ -70,7 +66,7 @@ func (bot *MyBot) Stop(){
 	bot.Pin7.Low()
 }
 
-func (bot *MyBot) TurnRight(){
+func (bot *MyBot) TurnRight() {
 	fmt.Println("Turning Right!")
 
 	bot.Pin15.High()
@@ -79,7 +75,7 @@ func (bot *MyBot) TurnRight(){
 	bot.Pin7.High()
 }
 
-func (bot *MyBot) TurnLeft(){
+func (bot *MyBot) TurnLeft() {
 	fmt.Println("Turning Left!")
 	bot.Pin15.Low()
 	bot.Pin11.High()
