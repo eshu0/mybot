81a3ff5c8d1bad2e6f810d3583fb69e93afbc25b
eshu0 <willsawesometoolkit@gmail.com>
2020-11-27 20:16:32 +0000

    Update RESTbot.go

--- rest/RESTbot.go
+++ rest/RESTbot.go
@@ -18,26 +18,32 @@ func NewRbot() Rbot {
 }
 
 func (bot Rbot) Stop(request Request.ServerRequest) {
+	request.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Println("Rbot - Stop")
 	bot.mbot.Stop()
 }
 
 func (bot Rbot) Forwards(request Request.ServerRequest) {
+	request.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Println("Rbot - Forwards")
 	bot.mbot.Forwards()
 }
 
 func (bot Rbot) Backwards(request Request.ServerRequest) {
+	request.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Println("Rbot - Backwards")
 	bot.mbot.Forwards()
 }
 
 func (bot Rbot) SpinLeft(request Request.ServerRequest) {
+	request.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
 	fmt.Println("Rbot - SpinLeft")
 	bot.mbot.SpinLeft()
 }
 
 func (bot Rbot) SpinRight(request Request.ServerRequest) {
+	request.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Println("Rbot - SpinRight")
 	bot.mbot.SpinRight()
 }
