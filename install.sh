6089d60f6652e66486ac9bd7d8f73ea9d45a328c
eshu0 <willsawesometoolkit@gmail.com>
2020-11-28 00:21:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cp /home/pi/go/src/github.com/eshu0/mybot/mybot.service /etc/systemd/system/mybo
  
 systemctl daemon-reload
 
-systemctl enable example.service
+systemctl enable mybot.service
 
 systemctl start mybot.service
 
