33b5575ac9483158d25f606a29e4a3b85f8b7f96
eshu0 <willsawesometoolkit@gmail.com>
2020-12-29 12:21:57 +0000

    Added comments

--- pkg/interfaces/imybot.go
+++ pkg/interfaces/imybot.go
@@ -1,18 +1,32 @@
 package ibot
 
-// main interface for the SimpleLogger
+// main interface for the IMyBot
 type IMyBot interface {
+
+	// Move Robot Fowards
 	Forwards()
+
+	// Move Robot Backwards
 	Backwards()
 
+	// Stop Robots Movements
 	Stop()
 
+	// Spin Robot Right
 	SpinRight()
+
+	// Spin Robot Left
 	SpinLeft()
 
+	// Close Connection
 	Close()
 
+	// Set Horizontal Flip of the Camera
 	Hflip(b bool)
+
+	// Set Vertical Flip of the Camera
 	Vflip(b bool)
+
+	// Returns fie path of camera and error if there was one
 	Capture() (string, error)
 }
